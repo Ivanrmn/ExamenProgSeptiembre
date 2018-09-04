d99c41b9cd64eb7f7bda6934f0641daea89db83b
ivanrmn <ivanrmnh@gmail.com>
2018-09-04 17:44:14 +0200

    Registro contieneEvento

--- pulpo/src/main/java/org/mvpigs/pulpo/registro/Registro.java
+++ pulpo/src/main/java/org/mvpigs/pulpo/registro/Registro.java
@@ -1,5 +1,23 @@
 package org.mvpigs.pulpo.registro;
 
+import java.util.List;
+
 public class Registro {
+
+    private List<String> eventos;
+    private Boolean pulpo;
+
+    public String contieneEvento(String evento, List<String> eventos) {
+        for (int i = 0; i < eventos.size(); i++) {
+            if (eventos.get(i) == evento) {
+                return evento + "está en la lista de eventos";
+            }
+        }
+        return null;
+    }
+
+    public void mostrarRegistro() {
+
+    }
 
 }
